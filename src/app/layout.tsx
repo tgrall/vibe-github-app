1bd6c788373ee78fb93e09f0c08531a5c3fcd143
Tugdual Grall <tgrall@github.com>
2025-06-04 13:23:54 +0200

    refactor: remove RootLayout component and redirect logic for default locale

--- src/app/layout.tsx
+++
@@ -1,9 +0,0 @@
-// This file is only needed to redirect root requests to the default locale
-// The actual layout is in [locale]/layout.tsx
-
-import {redirect} from 'next/navigation';
-
-export default function RootLayout() {
-  // This should only be reached when accessing "/" directly
-  redirect('/en');
-}
